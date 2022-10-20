bd320959139a8805f7361e559c21bfbffde5cd53
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-10-20 10:18:20 -0700

    added notes for my own understanding

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # connect to database using env variable
+# engine manages the overall connection to the database
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
+#Session generates temporary connections for CRUD operations
 Session = sessionmaker(bind=engine)
+#Base maps the models to the MySQL tables
 Base = declarative_base()
